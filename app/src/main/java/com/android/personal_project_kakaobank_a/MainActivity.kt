e6faf87bd139062f782831918ac767e0af9d14c7
choco5732 <choco5732@gmail.com>
2023-09-18 15:04:09 +0900

    Initail commit

---
+++ app/src/main/java/com/android/personal_project_kakaobank_a/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.android.personal_project_kakaobank_a
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
